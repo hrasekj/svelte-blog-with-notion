b57a2f66995501167d1c4f0cc3a5f5a64bf42450
Jakub Hrášek <hrasekj@gmail.com>
2023-07-18 21:13:12 +0200

    feat: add footer component to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
   import '../app.css';
+  // something wrong with eslint, when i use Footer instead of FooterComponent,
+  // the component as tag is lowercased.
+  import FooterComponent from '../components/footer/Footer.svelte';
 </script>
 
-<slot />
+<main class="flex flex-col h-screen justify-between">
+  <slot />
+  <FooterComponent />
+</main>
