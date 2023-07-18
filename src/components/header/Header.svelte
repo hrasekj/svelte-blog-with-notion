44171868bd185ff237eefba5d96abef811bd14de
Jakub Hrášek <hrasekj@gmail.com>
2023-07-18 21:17:47 +0200

    feat: add Header component

---
+++ src/components/header/Header.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let title: string;
+</script>
+
+<header
+  class="w-full m-0 p-0 bg-cover bg-bottom relative overflow-hidden h-[80vh] max-h-[300px] md:max-h-[600px]"
+>
+  <img
+    class="absolute w-full h-auto object-cover z-0"
+    src="/nature-landscape-water.jpg"
+    alt=""
+    width="1920"
+    height="1080"
+  />
+  <div class="container relative max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
+    <h1 class="text-black text-7xl md:text-9xl" style="font-family: cursive">{title}</h1>
+  </div>
+</header>
